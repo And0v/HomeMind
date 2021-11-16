8fe3f514c51b8697bea15eba53d11aa2fa07bf3c
And0v <and.ov@ukr.net>
2021-11-16 21:08:06 +0200

    first commit
    first commit

---
+++ HouseMindCore/build/classes-generated/org/housemind/core/options/Bundle.java
@@ -0,0 +1,19 @@
+package org.housemind.core.options;
+/** Localizable strings for {@link org.housemind.core.options}. */
+class Bundle {
+    /**
+     * @return <i>Advanced Options</i>
+     * @see AdvancedOptionsOptionsPanelController
+     */
+    static String AdvancedOption_DisplayName_AdvancedOptions() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "AdvancedOption_DisplayName_AdvancedOptions");
+    }
+    /**
+     * @return <i>Options</i>
+     * @see AdvancedOptionsOptionsPanelController
+     */
+    static String AdvancedOption_Keywords_AdvancedOptions() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "AdvancedOption_Keywords_AdvancedOptions");
+    }
+    private Bundle() {}
+}
---
+++ HouseMindCore/src/org/housemind/interfaces/HardwareInterface.java
@@ -0,0 +1,18 @@
+package org.housemind.interfaces;
+
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author and.ov
+ */
+public class HardwareInterface  {
+    
+    
+}
